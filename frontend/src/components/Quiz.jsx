a1fb90a9688bf8d24466c4723ed85bf0fd6b9d40
unknown <sharifrayan952@gmail.com>
2025-11-06 01:31:58 +0530

    Done

--- frontend/src/components/Quiz.jsx
+++ frontend/src/components/Quiz.jsx
@@ -237,9 +237,9 @@ const Quiz = ({ questions }) => {
 
   return (
     <>
-      <div className="flex justify-center sm:w-[100%] md:w-[70vw] items-center sm:absolute  md:absolute md:top-0 md:left-80  min-h-screen bg-gray-50 p-6">
-        <form className="w-[70vw]" onSubmit={(e) => handleSubmit(e)}>
-          <div className="w-full max-w-5xl bg-white rounded-lg shadow-lg h-[100%] p-3  md:p-6">
+      <div className="flex justify-center w-[100%] md:w-[70vw] items-center  sm:absolute  md:absolute md:top-0 md:left-80  min-h-screen bg-gray-50 p-6">
+        <form className="" onSubmit={(e) => handleSubmit(e)}>
+          <div className="w-[100%] bg-white rounded-lg shadow-lg h-[100%] p-3  md:p-6">
             {/* Header */}
             <div className="flex justify-between items-center mb-4">
               <h2 className="text-[14px] md:text-xl font-bold text-blue-600 ">
