3968f1abeb8626061469b14fec0e70781193d034
unknown <sharifrayan952@gmail.com>
2025-11-06 12:16:08 +0530

    Developmented

--- frontend/src/components/Bars1.jsx
+++ frontend/src/components/Bars1.jsx
@@ -78,7 +78,7 @@ const StudentFileCard = () => {
           <div className="w-auto h-55 flex justify-center items-center">
             <img className="w-60" src="errorico.png" alt="" />
           </div>
-          <p className="w-auto flex justify-center items-center">
+          <p className="w-auto text-center flex justify-center items-center">
             - Oop's, Does'nt have an any Assignment yet! -
           </p>
         </div>
--- frontend/src/components/Bars2.jsx
+++ frontend/src/components/Bars2.jsx
@@ -78,7 +78,7 @@ const StudentFileCard = () => {
           <div className="w-auto h-55 flex justify-center items-center">
             <img className="w-60" src="gdf.png" alt="" />
           </div>
-          <p className="w-auto flex justify-center items-center">
+          <p className="w-auto flex text-center justify-center items-center">
             - Oop's, Does'nt have an any Announcement yet! -
           </p>
         </div>
